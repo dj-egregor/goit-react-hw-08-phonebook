5b26c9c2225911a77a120d67a5869464072cf49c
Alexander Kushnir <dj.egregor1@gmail.com>
2023-03-02 21:54:47 +0100

    Update RegisterForm.jsx

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,85 +1,7 @@
-// import { RotatingLines } from 'react-loader-spinner';
-// import { ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import { useEffect } from 'react';
-
-// import { serverError, registerError } from 'utils/notification';
-// import { useRegisterMutation } from 'redux/userApi';
-// import css from './RegisterForm.module.css';
-
-// const RegisterForm = () => {
-//   const [register, { isLoading, error }] = useRegisterMutation();
-
-//   useEffect(() => {
-//     if (error) {
-//       if (error.status === 400) {
-//         registerError();
-//       }
-//       if (error.status === 500) {
-//         serverError();
-//       }
-//     }
-//   });
-
-//   const handleSubmit = e => {
-//     e.preventDefault();
-//     const form = e.currentTarget;
-//     const name = form.elements.name.value;
-//     const email = form.elements.email.value;
-//     const password = form.elements.password.value;
-
-//     register({ name, email, password });
-//   };
-
-//   return (
-//     <>
-//       <ToastContainer autoClose={1500} position="top-center" />
-//       <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
-//         <label className={css.label}>
-//           Username
-//           <input
-//             className={css.input}
-//             type="text"
-//             name="name"
-//             placeholder="Username"
-//             required
-//           />
-//         </label>
-//         <label className={css.label}>
-//           Email
-//           <input
-//             className={css.input}
-//             type="email"
-//             name="email"
-//             placeholder="Email"
-//             required
-//           />
-//         </label>
-//         <label className={css.label}>
-//           Password
-//           <input
-//             className={css.input}
-//             type="password"
-//             name="password"
-//             placeholder="Password"
-//             required
-//           />
-//         </label>
-//         <button className={css.btn} type="submit" disabled={isLoading}>
-//           {isLoading && <RotatingLines height="20" width="20" />}
-//           Register
-//         </button>
-//       </form>
-//     </>
-//   );
-// };
-
-// export default RegisterForm;
-
 import { RotatingLines } from 'react-loader-spinner';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import { serverError, registerError } from 'utils/notification';
 import { useRegisterMutation } from 'redux/userApi';
@@ -87,7 +9,6 @@ import css from './RegisterForm.module.css';
 
 const RegisterForm = () => {
   const [register, { isLoading, error }] = useRegisterMutation();
-  const [passwordError, setPasswordError] = useState(false);
 
   useEffect(() => {
     if (error) {
@@ -96,9 +17,6 @@ const RegisterForm = () => {
       }
       if (error.status === 500) {
         serverError();
-      }
-      if (error.status === 401) {
-        setPasswordError(true);
       }
     }
   }, [error]);
@@ -112,7 +30,6 @@ const RegisterForm = () => {
     const confirmPassword = form.elements.confirmPassword.value;
 
     if (password !== confirmPassword) {
-      setPasswordError(true);
       toast.error('Passwords do not match. Please try again.');
       return;
     }
@@ -169,8 +86,6 @@ const RegisterForm = () => {
           Register
         </button>
       </form>
-      {/* {passwordError &&
-        toast.error('Passwords do not match. Please try again.')} */}
     </>
   );
 };
